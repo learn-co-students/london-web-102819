1cd51cd7767e05131733869fe8ae14da4a4bdf12
Sam Barker <samuel.e.barker@googlemail.com>
2019-11-11 12:10:12 +0000

    after ruby hashes internet lecture

--- 07-hashes-and-the-internet/app.rb
+++ 07-hashes-and-the-internet/app.rb
@@ -2,8 +2,20 @@ require 'pry'
 require 'rest-client'
 require 'json'
 
-api_endpoint = 'https://swapi.co/api/people/1/'
+puts "give me a caharcter name"
+char_name_search_term = gets.chomp
 
+api_endpoint = "https://swapi.co/api/people/?search=#{char_name_search_term}"
+
+puts 'Loading pease wait'
 response = RestClient.get(api_endpoint)
 
-binding.pry
+hash = JSON.parse(response.body)
+
+def pretty_print_character_details(character_hash)
+    puts "Name: #{character_hash["name"]}"
+    puts "Mass: #{character_hash["mass"]} units???"
+    puts "Birth year: #{character_hash["birth_year"]}"
+end
+
+pretty_print_character_details(hash["results"][0])
