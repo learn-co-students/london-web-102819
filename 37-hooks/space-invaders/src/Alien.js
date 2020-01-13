8bef67a71c238b6c649531d944b256c651ee9cf3
Sam Barker <samuel.e.barker@googlemail.com>
2020-01-13 17:19:34 +0000

    37 hooks

--- 37-hooks/space-invaders/src/Alien.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Alien = () => {
-  return <img className="sprite" src={require("./imgs/alien.png")} />;
-};
-
-export default Alien;
--- 37-hooks/space-invaders/src/Player.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Player.css";
-
-const Player = () => {
-  return <img className="sprite player" src={require("./imgs/ship.png")} />;
-};
-
-export default Player;
